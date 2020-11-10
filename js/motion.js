7a1fc209ab27e153be22699aaf2e213be50337ef
SKETCH JIHWAN <yolo.jihwan@kakao.com>
2020-11-10 16:44:28 +0900

    finally

--- js/motion.js
+++ js/motion.js
@@ -52,5 +52,11 @@ $('#masonry-grid').masonry({
 	  $(this).addClass("active");
   });
 
-
-
+  //counter
+  const countEl = document.getElementById("count");
+updateVisitCount();
+function updateVisitCount() {
+	fetch("https://api.countapi.xyz/update/sketchwan/mysite?amount=1").then(res => res.json()).then(res => {
+		countEl.innerHTML = res.value;
+	});
+}
