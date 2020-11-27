69a069aa569ecdf10223e19060664dc1ab4efeb6
SKETCH JIHWAN <yolo.jihwan@kakao.com>
2020-11-27 11:42:29 +0900

    test

--- js/motion.js
+++ js/motion.js
@@ -32,33 +32,36 @@ filterItems.click(function(){
 });
 
 */
+$(window).load(function(){
+	
+	$('#masonry-grid').masonry({
+		columnWidth: 270,
+		itemSelector: '.grid-item'
+	});
+	
+	
+	// This does the filter by binding an event on the change of a select box
+	$("#grid-filter li").click(function() {
+		var group = $(this).data('category');
+		var group_class = "." + group;
+		
+		if(group == "*"){
+			$(".grid-item").show();
+			$('#masonry-grid').masonry('layout');
+			}
+		else if(group != "") {
+			$(".grid-item").hide();
+			$(group_class).show();
+			$('#masonry-grid').masonry('layout');
+		} else {
+			$(".grid-item").show();
+			$('#masonry-grid').masonry('layout');
+		}
+		$("#grid-filter li").removeClass("active");
+		$(this).addClass("active");
+	});
 
-$('#masonry-grid').masonry({
-	columnWidth: 270,
-	itemSelector: '.grid-item'
-  });
-  
-  
-  // This does the filter by binding an event on the change of a select box
-  $("#grid-filter li").click(function() {
-	  var group = $(this).data('category');
-	  var group_class = "." + group;
-	  
-	  if(group == "*"){
-		  $(".grid-item").show();
-		  $('#masonry-grid').masonry('layout');
-		 }
-	  else if(group != "") {
-		  $(".grid-item").hide();
-		  $(group_class).show();
-		  $('#masonry-grid').masonry('layout');
-	  } else {
-		  $(".grid-item").show();
-		  $('#masonry-grid').masonry('layout');
-	  }
-	  $("#grid-filter li").removeClass("active");
-	  $(this).addClass("active");
-  });
+});
 
 //modal
 
